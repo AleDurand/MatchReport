110b12219e370a231ea2d05f7841e510ec548826
Francisco Cuenca <kero4ever@gmail.com>
2016-09-26 21:56:24 -0300

    Revert "test del"
    This reverts commit 4957fbf2ef0f9699f943ccabd7a960eca9d4b261.

--- MatchReportAPI/src/test/java/project/controllers/ClubControllerTest.java
+++ MatchReportAPI/src/test/java/project/controllers/ClubControllerTest.java
@@ -1,4 +1,4 @@
-/*package project.controllers;
+package project.controllers;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -35,4 +35,3 @@ public class ClubControllerTest {
 	}
 
 }
-*/
