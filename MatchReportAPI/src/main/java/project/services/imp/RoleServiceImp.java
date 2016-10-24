95431a95f607900bf4cd6a0392b14643a7beedbd
Alejandro Durand <alejandro.durand.90@gmail.com>
2016-10-24 18:05:25 -0300

    Added roles endpoint.

---
+++ MatchReportAPI/src/main/java/project/services/imp/RoleServiceImp.java
@@ -0,0 +1,45 @@
+package project.services.imp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import project.exceptions.EntityNotFoundException;
+import project.models.RoleModel;
+import project.repositories.RoleRepository;
+import project.services.RoleService;
+
+@Service
+public class RoleServiceImp implements RoleService {
+
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Override
+	public RoleModel create(RoleModel role) {
+		return roleRepository.save(role);
+	}
+
+	@Override
+	public RoleModel read(Integer id) {
+		if (roleRepository.exists(id)) {
+			throw new EntityNotFoundException("resource.not_found", null);
+		}
+		return roleRepository.findOne(id);
+	}
+
+	@Override
+	public void delete(Integer id) {
+		if (roleRepository.exists(id)) {
+			throw new EntityNotFoundException("resource.not_found", null);
+		}
+		roleRepository.delete(id);
+	}
+
+	@Override
+	public List<RoleModel> getAll() {
+		return roleRepository.findAll();
+	}
+
+}
---
+++ MatchReportAPI/src/test/java/project/controllers/RoleControllerTest.java
@@ -0,0 +1,123 @@
+package project.controllers;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import project.Application;
+import project.exceptions.EntityNotFoundException;
+import project.models.RoleModel;
+import project.services.RoleService;
+
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = { Application.class })
+@AutoConfigureMockMvc
+public class RoleControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private GlobalExceptionController controllerAdvice;
+
+	@InjectMocks
+	private RoleController roleController;
+
+	@Mock
+	private RoleService roleServiceMock;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		mockMvc = MockMvcBuilders.standaloneSetup(roleController)
+				.setControllerAdvice(controllerAdvice).build();
+	}
+
+	@Test
+	public void read() throws Exception {
+		// @formatter:off
+		RoleModel expectedRole = new RoleModel();
+		expectedRole.setId(1);
+		expectedRole.setName("name");
+		when(roleServiceMock.read(1)).thenReturn(expectedRole);
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/roles/{id}", 1)
+			.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.id", is(1)))
+			.andExpect(jsonPath("$.name", is("name")))
+			.andExpect(content().contentType("application/json;charset=UTF-8"));
+		// @formatter:on
+	}
+
+	@Test
+	public void readNotFound() throws Exception {
+		// @formatter:off
+		when(roleServiceMock.read(1)).thenThrow(new EntityNotFoundException("resource.not_found", null));
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/roles/{id}", 1)
+			.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isNotFound())
+			.andExpect(content().contentType("application/json;charset=UTF-8"));
+		// @formatter:on
+	}
+
+	@Test
+	public void delete() throws Exception {
+		// @formatter:off
+		mockMvc.perform(MockMvcRequestBuilders.delete("/roles/{id}", 1)
+			.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isNoContent());
+		// @formatter:on
+	}
+
+	@Test
+	public void deleteNotFound() throws Exception {
+		// @formatter:off
+		doThrow(new EntityNotFoundException("resource.not_found", null)).when(roleServiceMock).delete(1);
+		
+		mockMvc.perform(MockMvcRequestBuilders.delete("/roles/{id}", 1)
+			.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isNotFound());
+		// @formatter:on
+	}
+
+	@Test
+	public void getAll() throws Exception {
+		// @formatter:off
+		List<RoleModel> expectedRoles = Arrays.asList(new RoleModel());
+		when(roleServiceMock.getAll()).thenReturn(expectedRoles);
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/roles")
+			.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+            .andExpect(jsonPath("$", hasSize(1)))
+			.andExpect(content().contentType("application/json;charset=UTF-8"));
+		// @formatter:on
+	}
+
+}
