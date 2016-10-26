92ac425648cb0d4858e9c23d0e9ffa3966849571
Alejandro Durand <alejandro.durand.90@gmail.com>
2016-10-26 16:25:44 -0300

    Added validations for the MatchModel

--- MatchReportAPI/src/main/java/project/validators/MatchValidator.java
+++ MatchReportAPI/src/main/java/project/validators/MatchValidator.java
@@ -1,5 +1,7 @@
 package project.validators;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -16,7 +18,39 @@ public class MatchValidator implements Validator {
 
 	@Override
 	public void validate(Object target, Errors errors) {
-		// TODO Auto-generated method stub
+		MatchModel match = (MatchModel) target;
+		
+		if (match.getDate() == null) {
+			errors.rejectValue("date", "match.date.not_null", "{match.date.not_null}");
+		} else {
+			if (!match.getDate().after(new Date())) {
+				errors.rejectValue("date", "match.date.after_today", "{match.date.after_today}");
+			}
+		}
+		
+		if (match.getStadium() == null) {
+			errors.rejectValue("stadium", "match.stadium.not_null", "{match.stadium.not_null}");
+		} else {
+			if (match.getStadium().getId() == null) {
+				errors.rejectValue("stadium.id", "match.stadium.id_not_null", "{match.stadium.id_not_null}");
+			}
+		}
+		
+		if (match.getLocal() == null) {
+			errors.rejectValue("local", "match.local.not_null", "{match.stadium.not_null}");
+		} else {
+			if (match.getLocal().getId() == null) {
+				errors.rejectValue("local.id", "match.local.id_not_null", "{match.stadium.id_not_null}");
+			}
+		}
+		
+		if (match.getVisitor() == null) {
+			errors.rejectValue("visitor", "match.visitor.not_null", "{match.visitor.not_null}");
+		} else {
+			if (match.getVisitor().getId() == null) {
+				errors.rejectValue("visitor.id", "match.visitor.id_not_null", "{match.visitor.id_not_null}");
+			}
+		}
 	}
 
 }
--- MatchReportAPI/src/test/java/project/validators/MatchValidatorTest.java
+++ MatchReportAPI/src/test/java/project/validators/MatchValidatorTest.java
@@ -3,6 +3,9 @@ package project.validators;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,7 +17,9 @@ import org.springframework.validation.BindException;
 import org.springframework.validation.ValidationUtils;
 
 import project.Application;
+import project.models.ClubModel;
 import project.models.MatchModel;
+import project.models.StadiumModel;
 
 
 @ActiveProfiles("test")
@@ -38,10 +43,155 @@ public class MatchValidatorTest {
 
 	@Test
 	public void isValid() {
+		StadiumModel stadium = new StadiumModel();
+		stadium.setId(1);
+		ClubModel local = new ClubModel();
+		local.setId(1);
+		ClubModel visitor = new ClubModel();
+		visitor.setId(1);
 		MatchModel match = new MatchModel();
+		match.setDate(new Date(new Date().getTime() + TimeUnit.DAYS.toMillis(1)));
+		match.setStadium(stadium);
+		match.setLocal(local);
+		match.setVisitor(visitor);
 		BindException errors = new BindException(match, "match");
 		ValidationUtils.invokeValidator(validator, match, errors);
 		assertFalse(errors.hasErrors());
+	}
+	
+	@Test
+	public void invalidDateNull() {
+		StadiumModel stadium = new StadiumModel();
+		stadium.setId(1);
+		ClubModel local = new ClubModel();
+		local.setId(1);
+		ClubModel visitor = new ClubModel();
+		visitor.setId(1);
+		MatchModel match = new MatchModel();
+		match.setDate(null);
+		match.setStadium(stadium);
+		match.setLocal(local);
+		match.setVisitor(visitor);
+		BindException errors = new BindException(match, "match");
+		ValidationUtils.invokeValidator(validator, match, errors);
+		assertTrue(errors.hasErrors());
+	}
+	
+	@Test
+	public void invalidDateBeforeThanToday() {
+		StadiumModel stadium = new StadiumModel();
+		stadium.setId(1);
+		ClubModel local = new ClubModel();
+		local.setId(1);
+		ClubModel visitor = new ClubModel();
+		visitor.setId(1);
+		MatchModel match = new MatchModel();
+		match.setDate(new Date(new Date().getTime() - TimeUnit.DAYS.toMillis(1)));
+		match.setStadium(stadium);
+		match.setLocal(local);
+		match.setVisitor(visitor);
+		BindException errors = new BindException(match, "match");
+		ValidationUtils.invokeValidator(validator, match, errors);
+		assertTrue(errors.hasErrors());
+	}
+	
+	@Test
+	public void invalidStadiumNull() {
+		ClubModel local = new ClubModel();
+		local.setId(1);
+		ClubModel visitor = new ClubModel();
+		visitor.setId(1);
+		MatchModel match = new MatchModel();
+		match.setDate(new Date(new Date().getTime() + TimeUnit.DAYS.toMillis(1)));
+		match.setStadium(null);
+		match.setLocal(local);
+		match.setVisitor(visitor);
+		BindException errors = new BindException(match, "match");
+		ValidationUtils.invokeValidator(validator, match, errors);
+		assertTrue(errors.hasErrors());
+	}
+	
+	@Test
+	public void invalidStadiumIdNull() {
+		StadiumModel stadium = new StadiumModel();
+		ClubModel local = new ClubModel();
+		local.setId(1);
+		ClubModel visitor = new ClubModel();
+		visitor.setId(1);
+		MatchModel match = new MatchModel();
+		match.setDate(new Date(new Date().getTime() + TimeUnit.DAYS.toMillis(1)));
+		match.setStadium(stadium);
+		match.setLocal(local);
+		match.setVisitor(visitor);
+		BindException errors = new BindException(match, "match");
+		ValidationUtils.invokeValidator(validator, match, errors);
+		assertTrue(errors.hasErrors());
+	}
+	
+	@Test
+	public void invalidLocalNull() {
+		StadiumModel stadium = new StadiumModel();
+		stadium.setId(1);
+		ClubModel visitor = new ClubModel();
+		visitor.setId(1);
+		MatchModel match = new MatchModel();
+		match.setDate(new Date(new Date().getTime() + TimeUnit.DAYS.toMillis(1)));
+		match.setStadium(stadium);
+		match.setLocal(null);
+		match.setVisitor(visitor);
+		BindException errors = new BindException(match, "match");
+		ValidationUtils.invokeValidator(validator, match, errors);
+		assertTrue(errors.hasErrors());
+	}
+	
+	@Test
+	public void invalidLocalIdNull() {
+		StadiumModel stadium = new StadiumModel();
+		stadium.setId(1);
+		ClubModel local = new ClubModel();		
+		ClubModel visitor = new ClubModel();
+		visitor.setId(1);
+		MatchModel match = new MatchModel();
+		match.setDate(new Date(new Date().getTime() + TimeUnit.DAYS.toMillis(1)));
+		match.setStadium(stadium);
+		match.setLocal(local);
+		match.setVisitor(visitor);
+		BindException errors = new BindException(match, "match");
+		ValidationUtils.invokeValidator(validator, match, errors);
+		assertTrue(errors.hasErrors());
+	}
+	
+	@Test
+	public void invalidVisitorNull() {
+		StadiumModel stadium = new StadiumModel();
+		stadium.setId(1);
+		ClubModel local = new ClubModel();
+		local.setId(1);
+		MatchModel match = new MatchModel();
+		match.setDate(new Date(new Date().getTime() + TimeUnit.DAYS.toMillis(1)));
+		match.setStadium(stadium);
+		match.setLocal(local);
+		match.setVisitor(null);
+		BindException errors = new BindException(match, "match");
+		ValidationUtils.invokeValidator(validator, match, errors);
+		assertTrue(errors.hasErrors());
+	}
+	
+	@Test
+	public void invalidVisitorIdNull() {
+		StadiumModel stadium = new StadiumModel();
+		stadium.setId(1);
+		ClubModel local = new ClubModel();
+		local.setId(1);
+		ClubModel visitor = new ClubModel();
+		MatchModel match = new MatchModel();
+		match.setDate(new Date(new Date().getTime() + TimeUnit.DAYS.toMillis(1)));
+		match.setStadium(stadium);
+		match.setLocal(local);
+		match.setVisitor(visitor);
+		BindException errors = new BindException(match, "match");
+		ValidationUtils.invokeValidator(validator, match, errors);
+		assertTrue(errors.hasErrors());
 	}
 
 }
