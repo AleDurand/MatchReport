679952f3f47ac8d2bb421a6408594f8b96806585
Alejandro Durand <alejandro.durand.90@gmail.com>
2016-10-26 12:15:05 -0300

    Added validations for the ClubModel.

--- MatchReportAPI/src/main/java/project/validators/ClubValidator.java
+++ MatchReportAPI/src/main/java/project/validators/ClubValidator.java
@@ -9,6 +9,9 @@ import project.models.ClubModel;
 @Component
 public class ClubValidator implements Validator {
 
+	public static final int MAXIMUM_NAME_LENGTH = 45;
+	public static final int MAXIMUM_ADDRESS_LENGTH = 45;
+	
 	@Override
 	public boolean supports(Class<?> clazz) {
 		return ClubModel.class.equals(clazz);
@@ -16,8 +19,32 @@ public class ClubValidator implements Validator {
 
 	@Override
 	public void validate(Object target, Errors errors) {
-		// TODO Auto-generated method stub
+		ClubModel club = (ClubModel) target;
 
+		if (club.getName() == null) {
+			errors.rejectValue("name", "club.name.not_null", "{club.name.not_null}");
+		} else {
+			if (club.getName().trim().isEmpty()) {
+				errors.rejectValue("name", "club.name.not_empty", "{club.name.not_empty}");
+			}
+			if (club.getName().length() > MAXIMUM_NAME_LENGTH) {
+				Object[] args = new Object[] { MAXIMUM_NAME_LENGTH };
+				errors.rejectValue("name", "club.name.size", args, "{club.name.size}");
+			}
+		}
+		
+		if (club.getAddress() == null) {
+			errors.rejectValue("address", "club.address.not_null", "{club.address.not_null}");
+		} else {
+			if (club.getAddress().trim().isEmpty()) {
+				errors.rejectValue("address", "club.address.not_empty", "{club.address.not_empty}");
+			}
+			if (club.getAddress().length() > MAXIMUM_ADDRESS_LENGTH) {
+				Object[] args = new Object[] { MAXIMUM_ADDRESS_LENGTH };
+				errors.rejectValue("address", "club.address.size", args, "{club.address.size}");
+			}
+		}
+		
 	}
 
 }
--- MatchReportAPI/src/test/java/project/validators/ClubValidatorTest.java
+++ MatchReportAPI/src/test/java/project/validators/ClubValidatorTest.java
@@ -15,6 +15,7 @@ import org.springframework.validation.ValidationUtils;
 
 import project.Application;
 import project.models.ClubModel;
+import project.utils.TestUtil;
 
 
 @ActiveProfiles("test")
@@ -39,9 +40,71 @@ public class ClubValidatorTest {
 	@Test
 	public void isValid() {
 		ClubModel club = new ClubModel();
+		club.setName("name");
+		club.setAddress("address");
 		BindException errors = new BindException(club, "club");
 		ValidationUtils.invokeValidator(validator, club, errors);
 		assertFalse(errors.hasErrors());
+	}
+	
+	@Test
+	public void invalidNameNull() {
+		ClubModel club = new ClubModel();
+		club.setName(null);
+		club.setAddress("address");
+		BindException errors = new BindException(club, "club");
+		ValidationUtils.invokeValidator(validator, club, errors);
+		assertTrue(errors.hasErrors());
+	}
+
+	@Test
+	public void invalidNameEmpty() {
+		ClubModel club = new ClubModel();
+		club.setName(" ");
+		club.setAddress("address");
+		BindException errors = new BindException(club, "club");
+		ValidationUtils.invokeValidator(validator, club, errors);
+		assertTrue(errors.hasErrors());
+	}
+
+	@Test
+	public void invalidNameTooLong() {
+		ClubModel club = new ClubModel();
+		club.setName(TestUtil.createStringWithLength(ClubValidator.MAXIMUM_NAME_LENGTH + 1));
+		club.setAddress("address");
+		BindException errors = new BindException(club, "club");
+		ValidationUtils.invokeValidator(validator, club, errors);
+		assertTrue(errors.hasErrors());
+	}
+	
+	@Test
+	public void invalidAddressNull() {
+		ClubModel club = new ClubModel();
+		club.setName("name");
+		club.setAddress(null);
+		BindException errors = new BindException(club, "club");
+		ValidationUtils.invokeValidator(validator, club, errors);
+		assertTrue(errors.hasErrors());
+	}
+
+	@Test
+	public void invalidAddressEmpty() {
+		ClubModel club = new ClubModel();
+		club.setName("name");
+		club.setAddress(" ");
+		BindException errors = new BindException(club, "club");
+		ValidationUtils.invokeValidator(validator, club, errors);
+		assertTrue(errors.hasErrors());
+	}
+
+	@Test
+	public void invalidAddressTooLong() {
+		ClubModel club = new ClubModel();
+		club.setName("name");
+		club.setAddress(TestUtil.createStringWithLength(ClubValidator.MAXIMUM_ADDRESS_LENGTH + 1));
+		BindException errors = new BindException(club, "club");
+		ValidationUtils.invokeValidator(validator, club, errors);
+		assertTrue(errors.hasErrors());
 	}
 
 }
